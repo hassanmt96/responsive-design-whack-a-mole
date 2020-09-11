99f7823f48710d731448dafe03ff5c99547fdfc6
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-09-11 15:07:22 -0500

    js-stuff

--- mole.js
+++ mole.js
@@ -1,1 +1,34 @@
-// Write your JS here.
+window.addEventListener('DOMContentLoaded', () => {
+    popUpRandomMole()
+  
+    const moleHeads = document.querySelectorAll('.wgs__mole-head');
+
+    for (let moleHead of moleHeads) {
+        moleHead.addEventListener("click", () => {
+        
+            moleHead.classList.add('wgs__mole-head--hidden');
+        })
+    }
+});
+
+let popUpRandomMole = () => {
+
+    const moleHeads = document.querySelectorAll('.wgs__mole-head');
+
+
+    const index = Math.floor(Math.random() * moleHeads.length);
+
+    const unhidden = moleHeads[index];
+
+    unhidden.classList.remove('wgs__mole-head--hidden');
+
+    setTimeout(() => hideMole(unhidden), 3000)
+}
+
+const hideMole = (mole) => {
+   
+    mole.classList.add('wgs__mole-head--hidden');
+   
+    
+    setTimeout(popUpRandomMole, 1000)
+}
